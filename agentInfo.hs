5caacbd3d5a86403931d9f792233397240924f95
Sumanth Kumar Mora <sumanthm@thoughtworks.com>
2017-01-14 23:14:03 +0530

    Create collection for Agent

--- agentInfo.hs
+++ agentInfo.hs
@@ -8,37 +8,71 @@ import Control.Lens hiding ((.=))
 import Network.Wreq
 import qualified Data.ByteString.Lazy as Lazy
 
-data Agents = Agents {
+
+data Agent = Agent {
       hostname          :: String
     , uuid              :: String
     , agentConfigState  :: String
 } deriving (Generic, Show)
 
-
-instance FromJSON Agents where
-    parseJSON = withObject "agents" $ \o -> do
+instance FromJSON Agent where
+    parseJSON = withObject "agent" $ \o -> do
         hostname <- o .: "hostname"
         uuid <- o .: "uuid"
         agentConfigState <- o .: "agent_config_state"
-        return Agents{..}
+        return Agent{..}
 
-instance ToJSON Agents where
-    toJSON Agents{..} = object [
+instance ToJSON Agent where
+    toJSON Agent{..} = object [
         "hostname" .= hostname,
         "uuid" .= uuid,
         "agent_config_state" .= agentConfigState ]
 
 
 
-getAgentsResponse :: IO Lazy.ByteString
-getAgentsResponse = do
+data Agents = Agents {
+      agents :: [Agent]
+} deriving (Generic, Show)
+
+instance FromJSON Agents where
+    parseJSON = withObject "agents" $ \o -> do
+        k <- parseJSON (Object o)
+        let agents = (k:[])
+        return Agents{..}
+
+{-
+instance ToJSON Agents where
+    toJSON Agents{..} = object [
+        "agents" .= toJSON (agent) ]
+
+    -}
+
+
+getAgentResponse :: IO Lazy.ByteString
+getAgentResponse = do
     let opts = defaults & header "Accept" .~ ["application/vnd.go.cd.v4+json"] & header "Content-Type" .~ ["application/json"]
     resp <- getWith opts "http://localhost:8153/go/api/agents/uuid-1"
     let y = resp ^. responseBody
     return y
 
 
+getAgentsResponse :: IO Lazy.ByteString
+getAgentsResponse = do
+    let opts = defaults & header "Accept" .~ ["application/vnd.go.cd.v4+json"] & header "Content-Type" .~ ["application/json"]
+    resp <- getWith opts "http://localhost:8153/go/api/agents"
+    let y = resp ^. responseBody
+    return y
+
+
+
 getAgentDetails = do
+    x <- getAgentResponse
+    let z = decode x :: Maybe Agent
+    return z
+
+
+getAgents = do
     x <- getAgentsResponse
     let z = decode x :: Maybe Agents
     return z
+
